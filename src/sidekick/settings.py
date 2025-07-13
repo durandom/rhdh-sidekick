739679d2862254774a60e9304b83fe02a526b0a2
Marcel Hild <hild@b4mad.net>
2025-07-13 11:19:52 +0200

    ignore env vars

--- src/sidekick/settings.py
+++ src/sidekick/settings.py
@@ -67,6 +67,7 @@ class Settings(BaseSettings):
         env_file_encoding="utf-8",
         case_sensitive=False,
         env_nested_delimiter="__",
+        extra="ignore",  # Allow extra env vars
     )
 
     # Application settings
